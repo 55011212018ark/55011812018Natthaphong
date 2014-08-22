0d9803fbdce544a06ddf361441a1fc1e796f0bc1
55011212088aungaing <imac@msu>
2014-08-22 15:16:54 +0700

    save

--- Array.playground/section-1.swift
+++ Array.playground/section-1.swift
@@ -30,3 +30,61 @@ var firstIem = shoppingList[0]
 //fiestItem is equal to "Eggs"
 shoppingList[4...6] = ["Bananas","Apples"]
 //shoppingList now contains 6 item
+
+
+
+//Dictionnary
+
+let array:Array<Int> = [1,2,3,4]
+let dictionnary:Dictionary<String,Int> = ["dog":1,"elephant":2]
+
+var airports: [String: String] = ["TYO":"Tokyo","DUB":"Dublin"]
+if  airports.isEmpty{
+    println("The airports dictionary is empty.")
+} else {
+    println("The airports dictionary is not empty.")
+}
+// prints "the airports dictionary is not empty."
+
+println("The airports dictionary contains \(airports.count) items.")
+//  prints "The airports dictionary contains 2 items."
+
+
+//Update and Add value Dictionary
+
+airports["LHR"] = "London"
+// the airports dictionary now contains 3 items
+
+
+
+airports["LHR"] = "London Heathrow"
+//the value for "LHR" has been changed to "London Heathow"
+
+
+if let oldValue = airports.updateValue("Dublin International", forKey: "DUB"){
+    println("The old value for DUB was \(oldValue).")
+}
+// prints "The old value for DUB was Dublin."
+
+
+//Remove value Dictionary
+
+airports["APL"] = "Apple International"
+    //
+    airports["APL"] = nil
+    //
+    
+if  let removedValue = airports.removeValueForKey("DUB") {
+    println("The removed airport's name is \(removedValue).")
+} else {
+    println("The airports dictionary dose not contain a value for DUB.")
+}
+//prints "the removed airport's name is Dublin Internaional
+
+
+
+
+
+
+
+
