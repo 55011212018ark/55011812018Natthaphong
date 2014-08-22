7c0b9e05c7f02571b9e53c94149e5cc286ec858c
55011212088aungaing <imac@msu>
2014-08-22 13:58:03 +0700

    var

--- pop.playground/section-1.swift
+++ pop.playground/section-1.swift
@@ -2,5 +2,20 @@
 
 import UIKit
 
-var str = "Hello, playground"
-let ca = 445
+let tutorialTeam = 56
+let editorialTeam = 23
+var totalTeam = tutorialTeam + editorialTeam
+
+totalTeam += 1
+
+let tutorialTeamInferred = -19
+let tutorialTeamExplicit: Int = -19
+
+let priceInferred = -19.99
+let priceExplicit:Double = -19.99
+
+let onSaleInferred = true
+let onSaleExplicit:Bool = false
+
+let nameInferred = "Whoopie Cushion"
+let nameExplicit:String = "Whoopie Cushion"
