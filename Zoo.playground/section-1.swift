13859bc0386416ba17e3c8eefe979c8db5b49dec
55011212182som <imac@msu>
2014-09-03 15:30:59 +0700

    ark

--- Zoo.playground/section-1.swift
+++ Zoo.playground/section-1.swift
@@ -1,25 +1,41 @@
-class Animal{
+class Zoo{
 
     let name:String
     let food:String
-    var nameAni:[String] = ["fox","monkey","camel","chicken"]
-    var foodAni:[String] = ["beef","banana","grass","rice"]
+    
+    var nameAni:[String] = ["fox","monkey","camel","chicken","elephane","panda"]
+    var foodAni:[String] = ["beef","banana","grass","rice","cane","bamboo"]
     init(name:String,food:String){
         self.name = name
         self.food = food
-        
             }
     
-    func add() ->String{
+    func add(){
             nameAni += name
             foodAni += food
         
-            return name+" "+food
-        
-            }
+        }
     
+    func searchAnimal(name:String)->String{
+        for(var i=0;i<nameAni.count;i++){
+            if(nameAni[i]==name){
+                var sting = foodAni[i]
+                return "food for "+name+" is "+sting+"."
+            }
+        }
+        return "No Animal on Zoo."
+        
+        }
 }
 
- let teatAdd = Animal(name: "rat", food: "corn")
-teatAdd.add()
+    
 
+
+let OnZoo = Zoo(name: "rat" ,food: "corn" )
+    OnZoo.add()
+    
+
+OnZoo.searchAnimal("fox")
+OnZoo.searchAnimal("dog")
+OnZoo.searchAnimal("bat")
+OnZoo.searchAnimal("panda")
